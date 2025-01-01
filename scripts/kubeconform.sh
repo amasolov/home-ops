b9679c946f594fe103a1579aad16afcf54a56abb
Alexey Masolov <alexey.masolov@gmail.com>
2025-01-02 02:49:51 +1100

    fix

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -12,7 +12,7 @@ kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
     "-skip"
-    "Secret,ReplicationSource,ReplicationDestination"
+    "Secret,ReplicationSource,ReplicationDestination,ExternalSecret"
     "-schema-location"
     "default"
     "-schema-location"
