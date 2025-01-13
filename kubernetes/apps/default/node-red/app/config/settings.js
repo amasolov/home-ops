5770a6da7c75413cca099c18412d872e4dd4021f
Alexey Masolov <alexey.masolov@gmail.com>
2025-01-13 17:12:52 +1100

    feat(default): adding apps

---
+++ kubernetes/apps/default/node-red/app/config/settings.js
@@ -0,0 +1,72 @@
+process.env.TZ = "Australia/Sydney";
+
+module.exports = {
+  adminAuth: {
+    type: "credentials",
+    users: [
+        {
+            username: "admin",
+            password: process.env.NODE_RED_PASSWORD,
+            permissions: "*"
+        }
+    ]
+  },
+
+  flowFile: "flows.json",
+  credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
+  flowFilePretty: true,
+
+  uiPort: process.env.PORT || 1880,
+
+  diagnostics: {
+    enabled: true,
+    ui: true,
+  },
+
+  runtimeState: {
+    enabled: false,
+    ui: false,
+  },
+
+  logging: {
+    console: {
+      level: "info",
+      metrics: false,
+      audit: false,
+    },
+  },
+
+  contextStorage: {
+    default: {
+      module: "localfilesystem",
+    },
+  },
+
+  exportGlobalContextKeys: false,
+
+  externalModules: {},
+
+  editorTheme: {
+    tours: false,
+
+    projects: {
+      enabled: false,
+      workflow: {
+        mode: "manual",
+      },
+    },
+
+    codeEditor: {
+      lib: "monaco",
+      options: {},
+    },
+  },
+
+  functionExternalModules: true,
+  functionGlobalContext: {},
+
+  debugMaxLength: 1000,
+
+  mqttReconnectTime: 15000,
+  serialReconnectTime: 15000,
+}
